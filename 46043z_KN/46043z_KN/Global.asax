f25ccde25935a9a0f54b6747b0c7bfc182f51eb1
AngelPenchev <angelpenchev79@gmail.com>
2019-09-12 09:53:59 +0300

    ASP1

---
+++ 46043z_KN/46043z_KN/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_46043z_KN.Global" Language="C#" %>
---
+++ 46043z_KN/46043z_KN/Global.asax.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace _46043z_KN
+{
+    public class Global : System.Web.HttpApplication
+    {
+        protected void Application_Start(object sender, EventArgs e)
+        {
+        }
+    }
+}
